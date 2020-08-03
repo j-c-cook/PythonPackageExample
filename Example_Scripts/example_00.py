ecabb1bc439e29af07e158350b3cfe254be6800d
j-c-cook <jack.cook@okstate.edu>
2020-08-02 23:45:22 -0500

    create an example script making use of different aspects related to the package

---
+++ Example_Scripts/example_00.py
@@ -0,0 +1,14 @@
+# Jack C. Cook
+# August 2, 2020
+
+from Package import module
+from Package import helloworld
+import CoolProp
+
+
+def main():
+    module.function()
+
+
+if __name__ == '__main__':
+    main()
