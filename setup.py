0efeceb1bd5ad615625e120c2a96cf9f1df614f5
j-c-cook <jack.cook@okstate.edu>
2020-08-02 23:59:35 -0500

    show the rigamarol associated with pip setup.py files

---
+++ setup.py
@@ -0,0 +1,40 @@
+# Jack C. Cook
+# August 2, 2020
+
+from setuptools import setup
+from setuptools.extension import Extension
+import numpy as np
+from Cython.Distutils import build_ext
+
+# Cython extensions
+extensions = [
+    Extension(
+        name='Package.helloworld',  # this will become <name>.so
+        sources=['Package/helloworld.pyx'],  # a list of the cython and cpp files
+        include_dirs=[np.get_include()],
+    )
+]
+
+
+def getreqs(fname):
+    """
+    Get the requirements list from the text file
+    JCC 03.10.2020
+    :param fname: the name of the requirements text file
+    :return: a list of requirements
+    """
+    file = open(fname)
+    data = file.readlines()
+    file.close()
+    return [data[i].replace('\n', '') for i in range(len(data))]
+
+
+setup(name='PythonPackageExample',
+      version='0.0.1',
+      packages=['Package'],
+      install_requires=getreqs('requirements.txt'),
+      ext_modules=extensions,
+      cmdclass={'build_ext': build_ext},
+      author='Jack C. Cook',
+      author_email='jack.cook@okstate.edu',
+      description='an example python package')
